eeb1bd9938bb3ce5403ddbc1071289663c8f6faa
Souvlaki42 <souvlaki420@gmail.com>
2025-05-01 16:29:48 +0300

    feat: a bunch of stuff about snake game
    * Added high score save to a file
    * Added play size relative to terminal size
    * Fixed a bug where food would spawn on top of the walls
    * Moved game's files to a new directory
    * Added a justfile to automate building and running the game with a
    simple command

--- other/snake-game/main.cpp
+++ other/snake-game/main.cpp
@@ -4,19 +4,12 @@
 #include <algorithm>
 #include <string>
 #include <format>
-
-// TODO: make snake die on wall collision
-// TODO: make food not spawn on walls
-// TODO: replace MAXRAND with window dimensions
-// TODO: save high score in a encrypted file
-// Maybe cross compile the game
-// IMPORTANT: g++ snake.cpp -lncursesw -std=c++23 -o snake
-
-#define MAXRAND 20
+#include <fstream>
 
 #define DELAY 100'000 // 0.1 seconds to microseconds
+#define HIGH_SCORE_FILE "highscore.txt"
 
-using std::vector, std::find, std::string, std::format;
+using std::vector, std::find, std::string, std::format, std::fstream, std::ios;
 
 struct Point
 {
@@ -27,12 +20,12 @@ struct Point
   }
 };
 
-void place_food(Point &food, const vector<Point> &snake)
+void place_food(Point &food, const vector<Point> &snake, const int PLAY_SIZE)
 {
   while (true)
   {
-    food.x = rand() % MAXRAND;
-    food.y = rand() % MAXRAND;
+    food.x = 1 + (rand() % (PLAY_SIZE - 2));
+    food.y = 1 + (rand() % (PLAY_SIZE - 2));
     if (find(snake.begin(), snake.end(), food) == snake.end())
       break;
   }
@@ -40,14 +33,30 @@ void place_food(Point &food, const vector<Point> &snake)
 
 void print_message_relative_to_center(Point dimensions, string msg, int rows_below_center = 0)
 {
-  mvprintw(dimensions.y / 2 + rows_below_center, (dimensions.y - msg.length()) / 2, "%s", msg.c_str());
+  int start_col = (dimensions.x - msg.length()) / 2;
+  if (start_col < 0)
+    start_col = 0;
+  mvprintw(dimensions.y / 2 + rows_below_center, start_col, "%s", msg.c_str());
 }
 
 int main()
 {
+
   srand(time(NULL));
 
+  // Open file only for reading initially
+  fstream read_file(HIGH_SCORE_FILE, ios::in);
+  int highscore = 0;
+
+  if (read_file.is_open())
+  {
+    read_file >> highscore;
+    read_file.close();
+  }
+
   WINDOW *win = initscr();
+  cbreak();
+  noecho();
   keypad(win, true);
   nodelay(win, true);
   curs_set(0);
@@ -55,11 +64,13 @@ int main()
   Point dimensions;
   getmaxyx(win, dimensions.y, dimensions.x);
 
-  vector<Point> snake = {{MAXRAND / 2, MAXRAND / 2}};
+  const int PLAY_SIZE = (dimensions.y / 10) * 10;
+
+  vector<Point> snake = {{PLAY_SIZE / 2, PLAY_SIZE / 2}};
 
   Point food;
   Point dir = {1, 0};
-  place_food(food, snake);
+  place_food(food, snake, PLAY_SIZE);
 
   int score = 0;
   bool game_over = false;
@@ -87,8 +98,8 @@ int main()
 
     Point new_head = {snake.front().x + dir.x, snake.front().y + dir.y};
 
-    if (new_head.x < 0 || new_head.x >= MAXRAND ||
-        new_head.y < 0 || new_head.y >= MAXRAND)
+    if (new_head.x < 0 || new_head.x >= PLAY_SIZE ||
+        new_head.y < 0 || new_head.y >= PLAY_SIZE)
     {
       game_over = true;
       break;
@@ -105,7 +116,7 @@ int main()
     if (new_head == food)
     {
       score++;
-      place_food(food, snake);
+      place_food(food, snake, PLAY_SIZE);
     }
     else
     {
@@ -114,12 +125,12 @@ int main()
 
     erase();
 
-    for (int i = 0; i <= MAXRAND; ++i)
+    for (int i = 0; i <= PLAY_SIZE; ++i)
     {
       mvaddch(0, i, '#');
-      mvaddch(MAXRAND, i, '#');
+      mvaddch(PLAY_SIZE, i, '#');
       mvaddch(i, 0, '#');
-      mvaddch(i, MAXRAND, '#');
+      mvaddch(i, PLAY_SIZE, '#');
     }
 
     for (size_t i = 0; i < snake.size(); ++i)
@@ -129,7 +140,7 @@ int main()
 
     mvaddch(food.y + 1, food.x + 1, '&');
 
-    mvprintw(MAXRAND + 2, 0, "Score: %d", score);
+    mvprintw(PLAY_SIZE + 2, 0, "Score: %d", score);
 
     refresh();
 
@@ -138,9 +149,23 @@ int main()
 
   erase();
 
+  if (score > highscore)
+  {
+    highscore = score;
+    // Open file for writing only when needed
+    fstream write_file(HIGH_SCORE_FILE, ios::out);
+    if (write_file.is_open())
+    {
+      write_file << highscore;
+      write_file.close();
+    }
+  }
+
   print_message_relative_to_center(dimensions, "GAME OVER!");
   print_message_relative_to_center(dimensions, format("Final Score: {}", score), 1);
+  print_message_relative_to_center(dimensions, format("High Score: {}", highscore), 2);
   print_message_relative_to_center(dimensions, "Press \"q\" to exit...", 3);
+
   nodelay(win, false);
   while (true)
   {
