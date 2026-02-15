da78142141ffda3c055a548d602fa244cee9bf88
Souvlaki42 <ilias@moulas.dev>
2026-02-15 14:12:48 +0200

    feat: more testing with assembly functions

--- semester-3/other/functions.asm
+++ semester-3/other/functions.asm
@@ -1,13 +1,23 @@
 .data
-number: .word 0x80000000
+number_a: .word 0x00000009
+number_b: .word 0x0000000b
 nl: .byte '\n'
+
+message: .asciiz "It worked!"
 
 .text
 .globl main
 
 main:
-lw $s7, number
-jal println_int
+li $v0, 1
+lw $t1, number_a
+lw $t2, number_b
+mult $t1, $t2
+jal println_mul
+multu $t1, $t2
+jal println_mul
+la $s7, message
+jal println
 j exit
 
 println:
@@ -29,16 +39,6 @@ lb $a0, nl
 li $v0, 11
 syscall
 jr $ra
-
-println_int:
-li $v0, 1
-move $a0, $s7
-syscall
-lb $a0, nl
-li $v0, 11
-syscall
-jr $ra
-
 
 exit:
 li $v0, 10
