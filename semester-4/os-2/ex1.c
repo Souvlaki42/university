5d8d9827355dd3669c15ec13539a2747ac724ee3
Souvlaki42 <ilias@moulas.dev>
2026-04-01 15:02:52 +0300

    refactor: no need to store wait pids for id, plus declare pid variables only when they are needed

--- semester-4/os-2/ex1.c
+++ semester-4/os-2/ex1.c
@@ -15,30 +15,28 @@ pid_t fork_log(int id) {
 }
 
 int main() {
-  pid_t pid1, pid2, pid3, pid4, pid5, pid6, wpid;
+  printf("I'm P0: pid=%d, parent_pid=%d\n", getpid(), getppid());
   int status;
 
-  printf("I'm P0: pid=%d, parent_pid=%d\n", getpid(), getppid());
-
-  pid1 = fork_log(1);
+  pid_t pid1 = fork_log(1);
   if (pid1 > 0) {
-    pid2 = fork_log(2);
+    pid_t pid2 = fork_log(2);
 
     if (pid2 > 0) {
-      while ((wpid = waitpid(pid1, &status, 0)) > 0)
+      while (waitpid(pid1, &status, 0) > 0)
         ;
       printf("Child process exitted with status: %d\n", status);
       execl("/bin/cat", "cat", __FILE__, NULL);
     }
 
     if (pid2 == 0) {
-      pid4 = fork_log(4);
+      pid_t pid4 = fork_log(4);
       if (pid4 == 0)
         return 0;
-      pid5 = fork_log(5);
+      pid_t pid5 = fork_log(5);
       if (pid5 == 0)
         return 0;
-      pid6 = fork_log(6);
+      pid_t pid6 = fork_log(6);
       if (pid6 == 0)
         return 0;
       return 0;
@@ -53,7 +51,7 @@ int main() {
       perror("pipe() failed");
       return 2;
     }
-    pid3 = fork_log(3);
+    pid_t pid3 = fork_log(3);
     if (pid3 == 0) {
       if (write(pipefd[1], "hello from your child", 21) != 21) {
         perror("pipe write() failed");
@@ -63,7 +61,7 @@ int main() {
     }
 
     if (pid3 > 0) {
-      while ((wpid = waitpid(pid3, &status, 0)) > 0)
+      while (waitpid(pid3, &status, 0) > 0)
         ;
       if (read(pipefd[0], buff, sizeof(buff)) <= 0) {
         perror("pipe read() failed");
