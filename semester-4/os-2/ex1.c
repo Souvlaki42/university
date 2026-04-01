8b621a4c4ecb8130d8ff5ec0957a40ea2ef9e33e
Souvlaki42 <ilias@moulas.dev>
2026-04-01 14:16:30 +0300

    fix: forgot to pass arg0, when changed execv to execl

--- semester-4/os-2/ex1.c
+++ semester-4/os-2/ex1.c
@@ -25,7 +25,7 @@ int main() {
       while ((wpid = waitpid(pid1, &status, 0)) > 0)
         ;
       printf("Child process exitted with status: %d\n", status);
-      execl("/bin/cat", __FILE__, NULL);
+      execl("/bin/cat", "cat", __FILE__, NULL);
     }
 
     if (pid2 == 0) {
